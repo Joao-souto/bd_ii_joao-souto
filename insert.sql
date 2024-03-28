3da8add16baba289d3b60236faf18c3b1dcf7984
João Souto <joaosouto0@outlook.com>
2024-03-28 11:52:34 -0300

    update

---
+++ insert.sql
@@ -0,0 +1,10 @@
+use pizzaria;
+insert into clientes
+(
+cod_cliente, nome, cep, endereco, telefone, email
+)
+values
+(2,'Adriano','01515000','Rua 121233','11976535178','adriano123@outlook.com'),
+(3,'Marcelo','01515000','Rua 112312323','11976535178','lordeceluis@gmail.com'),
+(4,'Larissa','01515000','Rua 1afdg23','11976535178','larissinha0@outlook.com'),
+(5,'Pedro','01515000','Rua 12dsfgsdf3','11976535178','pedroca0@outlook.com')
