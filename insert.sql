d152f9f24e0421fc338708cc905055735f2547f6
João Souto <joaosouto0@outlook.com>
2024-04-04 10:58:10 -0300

    Avaliação

--- insert.sql
+++ insert.sql
@@ -4,7 +4,7 @@ insert into clientes
 cod_cliente, nome, cep, endereco, telefone, email
 )
 values
-(2,'Adriano','01515000','Rua 121233','11976535178','adriano123@outlook.com'),
-(3,'Marcelo','01515000','Rua 112312323','11976535178','lordeceluis@gmail.com'),
-(4,'Larissa','01515000','Rua 1afdg23','11976535178','larissinha0@outlook.com'),
-(5,'Pedro','01515000','Rua 12dsfgsdf3','11976535178','pedroca0@outlook.com')
+(2,'Adriano',01515000,'Rua 121233',11976535190,'adriano123@outlook.com'),
+(3,'Marcelo',01515000,'Rua 112312323',11976535178,'lordeceluis@gmail.com'),
+(4,'Larissa',01515000,'Rua 1afdg23',11976535177,'larissinha0@outlook.com'),
+(5,'Pedro',01515000,'Rua 12dsfgsdf3',11976535148,'pedroca0@outlook.com')
