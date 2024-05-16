9b906cfce401345bc6c4daa4a5e5ab62d1c0e43b
João Souto <joaosouto0@outlook.com>
2024-05-16 11:30:27 -0300

    Iniciando organização

--- primeiroSemestre/insert.sql
+++ primeiroSemestre/insert.sql
@@ -1,10 +1,10 @@
-use pizzaria;
-insert into clientes
+use pizzaria; -- comando para indicar qual banco de dados utilizar
+insert into clientes -- comando para referenciar qual entidade deste banco
 (
-cod_cliente, nome, cep, endereco, telefone, email
+cod_cliente, nome, cep, endereco, telefone, email -- quais campos receberam dados
 )
 values
-(2,'Adriano',01515000,'Rua 121233',11976535190,'adriano123@outlook.com'),
+(2,'Adriano',01515000,'Rua 121233',11976535190,'adriano123@outlook.com'), -- valores para respectivos campos
 (3,'Marcelo',01515000,'Rua 112312323',11976535178,'lordeceluis@gmail.com'),
 (4,'Larissa',01515000,'Rua 1afdg23',11976535177,'larissinha0@outlook.com'),
 (5,'Pedro',01515000,'Rua 12dsfgsdf3',11976535148,'pedroca0@outlook.com')
