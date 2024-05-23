d9922d29006052cd80b30392060d0e8842331b09
João Souto <joaosouto0@outlook.com>
2024-05-23 07:50:56 -0300

    atualizando

--- primeiroSemestre/restaurante/populandoBanco.sql
+++ primeiroSemestre/restaurante/populandoBanco.sql
@@ -6,19 +6,19 @@ values()
 insert into Filiais(Endereco,Email,Telefone,Quant_mesas,Avaliacao) --1
 values()
 
-insert into Funcionarios(CPF,Nome,Sexo,Idade,Endereco,Email,Telefone,ID_filial,Cargo,Data_contatacao) --5
+insert into Funcionarios(CPF,Nome,Sexo,Idade,Endereco,Email,Telefone,Cargo,Data_contatacao) --5
 values()
 
-insert into Reservas(Data_reserva,ID_cliente,Mesa,ID_filial,) --3
+insert into Reservas(Data_reserva,Mesa,Capacidade) --3
 values()
 
-insert into Fornecedores(Nome,ID_fornecedor,CNPJ,Tipo_material,Email,Telefone,Valor_material) --2
+insert into Fornecedores(Nome,CNPJ,Tipo_material,Email,Telefone,Valor_material) --2
 values()
 
-insert into Estoque(ID_materia,Quantidade,ID_filial) --material do prato
+insert into Estoque(Quantidade) --material do prato
 values()
 
-insert into Ingredientes(Unidade_medida) --
+insert into Ingredientes(Nome, Unidade_medida) --
 values()
 
 insert into Pratos(Nome,Descricao,Valor) --1
@@ -27,8 +27,8 @@ values()
 insert into Bebidas(Nome,Descricao,Valor) --2
 values()
 
-insert into Pedidos(ID_cliente,ID_prato,ID_bebida,Valor,Tipo_pagamento,Endereco) --5
+insert into Pedidos(Valor,Tipo_pagamento,Endereco,Data_pedido) --5
 values()
 
-insert into Entregas(Endereco,ID_pedido) --3
+insert into Entregas(Endereco,Data_entrega) --3
 values()
