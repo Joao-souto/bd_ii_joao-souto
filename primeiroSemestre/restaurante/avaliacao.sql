5b36c3b4169b99f19584653e164c24a78c656ddd
João Souto <joaosouto0@outlook.com>
2024-06-20 10:39:33 -0300

    Script de consulta finalizado.

--- primeiroSemestre/restaurante/avaliacao.sql
+++ primeiroSemestre/restaurante/avaliacao.sql
@@ -1,7 +1,16 @@
--- DESENVOLVA OS SCRIPTS CONFORME OS PROBLEMAS
+-- João Pedro Monteiro Souto
 
---Quais dados são necessários para o login?
+-- Quais dados são necessários para o login?
+SELECT * FROM Login WHERE Username = 'joaosouza' AND Senha = 'senha234'; -- SELECT * FROM Login WHERE Username = @username AND Senha = @senha; Também poderia ser utilizado se usado a partir de uma aplicação
+
 -- "" "" "" para vizualizar o cadastro do cliente
+SELECT * FROM Clientes WHERE ID_cliente = 1;
+
 -- Quantos funcionários existem por filial? Liste apenas os nomes
+SELECT Nome FROM Funcionarios Where ID_filial = '4';
+
 -- Quantos clientes existem por filial? Liste os clientes ordenados por filial
+SELECT Nome, ID_filial FROM Clientes ORDER BY ID_filial ASC;
+
 -- Elabore script de acordo com os requisitos de sua escolha
+SELECT * FROM Pratos Where Valor > 30.00; -- Filtrando pratos acima de 30 reais
